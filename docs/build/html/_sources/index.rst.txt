8aa31ed126810251a6db8e54b461f9a50e15d9a1
GUILLAUME PICHON <gpicho02@i4-02-00>
2023-10-31 14:48:02 +0100

    mise en place de la documentation

---
+++ docs/build/html/_sources/index.rst.txt
@@ -0,0 +1,20 @@
+.. Météo d'un aéroport documentation master file, created by
+   sphinx-quickstart on Tue Oct 31 14:44:31 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Météo d'un aéroport's documentation!
+===============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ docs/source/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Météo d'un aéroport"
+copyright = '2023, GUILLAUME PICHON; PROMISSE MIANSIANTIMA'
+author = 'GUILLAUME PICHON; PROMISSE MIANSIANTIMA'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'fr'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
