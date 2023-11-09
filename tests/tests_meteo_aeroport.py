faa0ea82faa24dc6e533abc05cf8f03b3c388311
etudiant <etudiant@deb12-vdi-rt>
2023-11-09 10:17:41 +0100

    Debut des tests python

---
+++ tests/tests_meteo_aeroport.py
@@ -0,0 +1,31 @@
+def genere_page_web(nom_fichier, titre_page, corps):
+    f = open(nom_fichier,'w',encoding='utf-8')
+    HTML_INDEX = """ <!DOCTYPE html>
+    <html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"fr\" lang=\"fr\" dir=\"ltr\">
+    <style>
+        table, th, td {
+        border:1px solid black;
+        }
+    </style>
+    <head>
+    <title> Mon titre </title>
+    </head>
+    <body>
+    Bonjour HTML
+    </body>
+    </html>
+    """
+    f.write(HTML_INDEX)
+    f.close()
+
+def main():
+    corps = """ <h1> Titre </h1>
+            """
+    genere_page_web("../html/index.html", "mon titre", corps)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
