6aaf68738604bd024c5e41c42879e5b2bc2bd73e
etudiant <etudiant@deb12-vdi-rt>
2023-12-04 23:01:24 +0100

    Test de scrapping

--- tests/scrapsite.py
+++ tests/scrapsite.py
@@ -1,19 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://fr.allmetsat.com/metar-taf/france.php?icao=LFBI"
 
-response = requests.get(url)
+url = "https://fr.allmetsat.com/metar-taf/france.php?icao=LFBI"
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+response = requests.get(url, headers=headers)
+print(response)
 
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, 'html.parser')
 
+    # wind_direction = soup.find("div", class_="mt").text.split()[1]
+    # wind_speed = soup.find("div", class_="mt").text.split()[2]
+    # temperature = soup.find("div", class_="mt").text.split()[4][:-1]
+    # humidity = soup.find("div", class_="mt").text.split()[6][:-1] 
+    # pressure = soup.find("div", class_="mt").text.split()[8]
+    # cloud_density = soup.find("div", class_="mt").text.split()[12]
+    # visibility = soup.find("div", class_="mt").text.split()[-3]
+    
     wind_direction = soup.find(text="Vent").find_next('b').text
     wind_speed = soup.find(text="Vent").find_next('b').find_next('b').text
     temperature = soup.find(text="Température").find_next('b').text
     humidity = soup.find(text="Humidité").find_next('b').text
     pressure = soup.find(text="Pression").find_next('b').text
-    clouds = soup.find(text="Nuages").find_next('b').text
+    cloud_density = soup.find(text="Nuages").find_next('b').text
     visibility = soup.find(text="Visibilité").find_next('b').text
 
     print(f"Direction du vent : {wind_direction}")
@@ -21,17 +31,11 @@ if response.status_code == 200:
     print(f"Température : {temperature}°C")
     print(f"Taux d'humidité : {humidity}%")
     print(f"Pression atmosphérique : {pressure} hPa")
-    print(f"Densité des nuages : {clouds}")
+    print(f"Densité des nuages : {cloud_density}")
     print(f"Visibilité : {visibility}")
 else:
     print(f"Échec de la requête avec le code de statut : {response.status_code}")
 
 
 
-# wind_direction = soup.find("div", class_="mt").text.split()[1]
-# wind_speed = soup.find("div", class_="mt").text.split()[2]
-# temperature = soup.find("div", class_="mt").text.split()[4][:-1]  # Enlever le symbole °C
-# humidity = soup.find("div", class_="mt").text.split()[6][:-1]  # Enlever le symbole %
-# pressure = soup.find("div", class_="mt").text.split()[8]
-# cloud_density = soup.find("div", class_="mt").text.split()[12]
-# visibility = soup.find("div", class_="mt").text.split()[-3]
+
