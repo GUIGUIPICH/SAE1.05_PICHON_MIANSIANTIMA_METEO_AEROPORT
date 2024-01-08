46b56e7e51954e51e394bf80f009c0878d55b4a3
Guillaume <116353926+GUIGUIPICH@users.noreply.github.com>
2024-01-08 17:04:57 +0100

    Update meteo_aeroport.py

--- meteo_aeroport/meteo_aeroport.py
+++ meteo_aeroport/meteo_aeroport.py
@@ -39,15 +39,15 @@ def main():
     airportname_complete, wind_speed, wind_direction, temperature, humidity, pressure, visibility, clouds = today
         
     #Mise à jour des variables pour Demain
-    tomorrow = weatherforecast(1, sitemeteoblue)
+    tomorrow = weatherforecast(2, sitemeteoblue)
     forecast1, temp_max1, temp_min1, wind_speed1, wind_direction1, precipitation1, pressure1 = tomorrow
             
     #Mise à jour des variables pour J+2
-    d2 = weatherforecast(2, sitemeteoblue)
+    d2 = weatherforecast(3, sitemeteoblue)
     forecast2, temp_max2, temp_min2, wind_speed2, wind_direction2, precipitation2, pressure2 = d2
         
     #Mise à jour des variables pour J+3
-    d3 = weatherforecast(3, sitemeteoblue)
+    d3 = weatherforecast(4, sitemeteoblue)
     forecast3, temp_max3, temp_min3, wind_speed3, wind_direction3, precipitation3, pressure3 = d3
     
     #Récupération de la date et de l'heure
